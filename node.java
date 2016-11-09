d3047c7bfa98d08f182f2c042eba0bc631931d3d
naeemkhan12 <naeemr2014@namal.edu.pk>
2016-11-09 12:52:50 +0500

    implementation of double linked list
    working on it .... few logical errors

---
+++ node.java
@@ -0,0 +1,21 @@
+
+public class node {
+	int data;
+	public node next;
+	public node previous;
+	public node()
+	{
+		data=0;
+		next = null;
+		previous=null;
+	}
+	public node(int i)
+	{
+		data=i;
+		next = null;
+		previous=null;
+	}
+
+
+	
+}
